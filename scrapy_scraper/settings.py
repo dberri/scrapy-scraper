f9c0275861771ad4da1f1b6288363f863c1705bf
David Berri <dwbwill@gmail.com>
2017-06-27 13:32:21 -0300

    settings updated

--- scrapy_scraper/settings.py
+++ scrapy_scraper/settings.py
@@ -95,7 +95,7 @@ ROBOTSTXT_OBEY = True
 import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 SPIDER_URL = {}
-spiders = ['black', 'blue', 'gold', 'pink', 'silver', 'yellow']
+spiders = ['black', 'blue', 'gold', 'pink', 'silver', 'yellow', 'brown']
 with open(dir_path+'/urls.txt', 'r') as f:
     for spider in spiders:
         SPIDER_URL[spider] = f.readline().strip()
