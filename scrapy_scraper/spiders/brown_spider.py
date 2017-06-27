567710c4a5703f8e24b78e1c732fcb42fe056b98
David Berri <dwbwill@gmail.com>
2017-06-27 13:44:01 -0300

    defensive brown spider

--- scrapy_scraper/spiders/brown_spider.py
+++ scrapy_scraper/spiders/brown_spider.py
@@ -16,31 +16,37 @@ class BlackSpider(scrapy.Spider):
 
     def parse_model(self, response):
 
-        def extract_all_with_xpath(query):
-            return response.xpath(query).extract()
+        def extract_figures(query):
+            try:
+                return response.xpath(query).extract()
+            except:
+                logging.warning("No PHOTO extracted")
 
-        def extract_with_xpath(query):
-            logging.info(response.url)
-            return response.xpath(query).extract_first().strip()
+        def extract_name(query):
+            try:
+                return response.xpath(query).extract_first().strip()
+            except:
+                logging.warning('No NAME extracted')
+                logging.warning(response.url)
+
 
         def extract_phone():
             result = response.xpath('//span[@itemprop="telephone"]/a/text()').extract_first()
             if result is not None:
                 return result.strip()
 
-            elif response.xpath('//a[contain(@href,"tel")/text()').extract_first():
-                return response.xpath('//a[contain(@href,"telephone")/text()').extract_first().strip()
+            elif response.xpath('//a[contains(@href,"tel")]/text()').extract_first():
+                return response.xpath('//a[contains(@href,"tel")]/text()').extract_first().strip()
 
             else:
-                logging.warning('No phone extracted!')
+                logging.warning('No PHONE extracted!')
 
 
 
         item = ModelItem()
-        item['name'] = extract_with_xpath('//span[@itemprop="name"]/text()')
+        item['name'] = extract_name('//span[@itemprop="name"]/text()')
         item['phone'] = extract_phone()
-        logging.info(item['phone'])
-        # item['photoLinks'] = extract_all_with_xpath('//figure/a/@href')
+        item['photoLinks'] = extract_figures('//figure/a/@href')
         # 'city': extract_with_css('p.cidade-modelo::text')
         # 'profile': extract_profile_with_xpath('//li[@class="opcao-submenu"]/span[1]/following-sibling::text()[1]')
         yield item
