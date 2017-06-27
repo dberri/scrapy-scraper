0f34857ffc02229fa2231e133b611001395d568a
David Berri <dwbwill@gmail.com>
2017-06-27 14:37:18 -0300

    fixed yellow spider

--- scrapy_scraper/spiders/yellow_spider.py
+++ scrapy_scraper/spiders/yellow_spider.py
@@ -13,18 +13,10 @@ class YellowSpider(scrapy.Spider):
 
     def parse_page(self, response):
         # follow links to author pages
-        for href in response.css('div.anuncio a::attr(href)'):
-            print(href)
+        for href in response.xpath('//div[@id="resultados"]/div/div/a[1]/@href'):
             yield response.follow(href, self.parse_model)
 
     def parse_model(self, response):
-        def extract_with_css(query):
-            result = response.css(query).extract_first()
-            if result is not None:
-                return result.strip()
-            else:
-                logging.warning(response.url)
-
         def extract_all_with_xpath(query):
             return response.xpath(query).extract()
 
@@ -35,18 +27,11 @@ class YellowSpider(scrapy.Spider):
             else:
                 logging.warning(response.url)
 
-        def extract_profile_with_xpath(query):
-            keys = ['age', 'height', 'weight', 'manequim']
-            profile = {}
-            profile_items = response.xpath(query).extract()
-            for key, item in zip(keys, profile_items):
-                profile[key] = item.strip()
-            return profile
-
         item = ModelItem()
-        item['name'] = extract_with_xpath('//span[@id="anuncio-nombre"]/text()[1]')
-        item['phone'] = extract_with_css('span.telephone::text')
-        # item['photoLinks'] =
+        item['name'] = extract_with_xpath('//span[@id="anuncio_nombre"]/text()')
+        item['phone'] = extract_with_xpath('//span[@itemprop="telephone"]/text()')
+        item['photoLinks'] = extract_all_with_xpath('//div[@id="anuncio_fotos"]//img/@src')
         # 'place': extract_with_xpath('//span[@id="anuncio-poblacion"]/text()[1]'),
-
         yield item
+
+# implement check if isn't in the database
